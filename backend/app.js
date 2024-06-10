49e194dc7870c32a7a533ab9d23442bf08e52a0f
AzmayenGalib <98214380+AzmayenGalib@users.noreply.github.com>
2024-06-11 02:06:39 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(express.json({limit:'50mb'}));
 app.use(cookieParser());
 app.use(cors(  {
        credentials:true ,  
-   /*  origin: 'http://localhost:3000', */
+     origin: 'http://localhost:3000', 
 }  ));
 
 app.use(bodyParser.json({limit:'50mb'}));
